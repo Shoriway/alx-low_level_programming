ac15fd5140c7a5f9f1e44ee3bcfd2fabc43dcf2e
Shoriway <shoriway@gmail.com>
2023-02-23 09:35:56 -0800

    moreNumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,13 +3,12 @@
 /**
  * more_numbers-  prints 10 times the numbers, from 0 to 14
  * followed by a new line
- * Return: 0
+ * Return: void
  */
-
 void more_numbers(void)
 {
 int i, j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
